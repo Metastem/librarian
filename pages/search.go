313f96e72c38b14a192d2b81f6dc018156773616
video-prize-ranch <cb.8a3w5@simplelogin.co>
2021-10-05 16:26:37 -0400

    Fix CSP for Tor hidden services

--- pages/search.go
+++ pages/search.go
@@ -18,7 +18,7 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("X-Content-Type-Options", "nosniff")
 	w.Header().Add("Strict-Transport-Security", "max-age=31557600")
 	w.Header().Add("Permissions-Policy", "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()")
-	w.Header().Add("Content-Security-Policy", "default-src 'none'; style-src 'self'; img-src 'self'; font-src 'self'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content; manifest-src 'self'")
+	w.Header().Add("Content-Security-Policy", "default-src 'none'; style-src 'self'; img-src 'self'; font-src 'self'; form-action 'self'; block-all-mixed-content; manifest-src 'self'")
 
 	page := 1
 	pageParam, err := strconv.Atoi(r.URL.Query().Get("page"))
@@ -48,10 +48,10 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 		"videos":   videoResults,
 		"channels": channelResults,
 		"query": map[string]interface{}{
-			"query": 		 query,
-			"page":      fmt.Sprint(page),
-			"nextPage":  fmt.Sprint(page + 1),
-			"prevPage":  fmt.Sprint(page - 1),
+			"query":    query,
+			"page":     fmt.Sprint(page),
+			"nextPage": fmt.Sprint(page + 1),
+			"prevPage": fmt.Sprint(page - 1),
 		},
 	})
 }
