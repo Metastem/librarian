88c4e7061c92af0a7fd5539b4148cd66facf1c6d
video-prize-ranch <cb.8a3w5@simplelogin.co>
2021-12-31 11:41:46 -0500

    Increase http/3 connection timeout

--- api/http3.go
+++ api/http3.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/spf13/viper"
@@ -9,6 +10,7 @@ import (
 
 var Client = &http.Client{
 	Transport: &http3.RoundTripper{},
+	Timeout: 45 * time.Second,
 }
 
 func CheckUseHttp3() {
