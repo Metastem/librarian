12ccb0e2bd42c8e85ab2e153c15e11fdd8491199
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-01-05 19:34:15 -0500

    Retry HTTP requests

--- api/http3.go
+++ api/http3.go
@@ -4,9 +4,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/hashicorp/go-retryablehttp"
 	"github.com/lucas-clemente/quic-go/http3"
 	"github.com/spf13/viper"
 )
+
+var retryClient = retryablehttp.NewClient()
 
 var Client = &http.Client{
 	Transport: &http3.RoundTripper{},
@@ -17,4 +20,8 @@ func CheckUseHttp3() {
 	if viper.GetBool("USE_HTTP3") {
 		Client = &http.Client{}
 	}
+
+	retryClient.RetryMax = 4
+	retryClient.HTTPClient = Client
+	Client = retryClient.StandardClient()
 }
