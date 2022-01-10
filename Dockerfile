64bebeefdddc84f5f15c46ad6e78d56fea297d5e
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-01-10 17:51:38 -0500

    Fix docker builds

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,11 @@ WORKDIR /src
 RUN apk --no-cache add git
 RUN git clone https://codeberg.org/librarian/librarian .
 
-ARG GOARCH=amd64
-RUN env GOARCH=${GOARCH} go build
-RUN mkdir /var/cache/librarian
+RUN go build
 
-FROM scratch as bin
+FROM alpine:latest as bin
 
 WORKDIR /app
-COPY --from=build /var/cache/librarian /var/cache/librarian
 COPY --from=build /src/librarian .
 
 EXPOSE 3000
--- build.sh
+++ build.sh
@@ -11,11 +11,5 @@ GOOS=openbsd GOARCH=amd64 go build
 tar -cf librarian-${VERSION}-openbsd-amd64.tar.gz librarian
 rm librarian
 
-docker build -t nineteengladespool/librarian:amd64 --build-arg GOARCH=amd64 .
-docker push nineteengladespool/librarian:amd64
-docker build -t nineteengladespool/librarian:arm64 --build-arg GOARCH=arm64 .
-docker push nineteengladespool/librarian:arm64
-docker manifest create nineteengladespool/librarian:latest --amend nineteengladespool/librarian:amd64 --amend nineteengladespool/librarian:arm64 
-docker manifest create nineteengladespool/librarian:${VERSION} --amend nineteengladespool/librarian:amd64 --amend nineteengladespool/librarian:arm64
-docker manifest push nineteengladespool/librarian:latest
-docker manifest push nineteengladespool/librarian:${VERSION}
+sudo docker buildx create --use
+sudo docker buildx build --platform linux/amd64,linux/arm64 -t nineteengladespool:latest -t nineteengladespool:${VERSION} --push .
