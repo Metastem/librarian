8875db41e6b358dbec725ef3fdb2288c39bbc320
video-prize-ranch <cb.8a3w5@simplelogin.co>
2022-04-10 21:20:50 -0400

    Add option to use external livestream proxy

--- api/live.go
+++ api/live.go
@@ -11,6 +11,7 @@ import (
 	"codeberg.org/librarian/librarian/types"
 	"codeberg.org/librarian/librarian/utils"
 	"github.com/dustin/go-humanize"
+	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
 )
 
@@ -39,12 +40,17 @@ func GetLive(claimId string) (types.Live, error) {
 	thumbnail = url.QueryEscape(thumbnail)
 	thumbnail = "/image?url=" + thumbnail + "&hash=" + utils.EncodeHMAC(thumbnail)
 
+	streamUrl := strings.ReplaceAll(data.Get("data.url").String(), "https://cdn.odysee.live", "/live")
+	if viper.GetString("LIVE_STREAMING_URL") != "" {
+		streamUrl = strings.ReplaceAll(data.Get("data.url").String(), "https://cdn.odysee.live", viper.GetString("LIVE_STREAMING_URL"))	
+	}
+
 	return types.Live{
 		ClaimId: data.Get("data.claimId").String(),
 		RelTime: humanize.Time(timestamp),
 		Time: timestamp.Format("Jan 2, 2006 03:04 PM"),
 		ThumbnailUrl: thumbnail,
-		StreamUrl: strings.ReplaceAll(data.Get("data.url").String(), "https://cdn.odysee.live", "/live"),
+		StreamUrl: streamUrl,
 		Live: data.Get("data.live").Bool(),
 	}, nil
 }
